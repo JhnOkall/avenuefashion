225e17bc26f35b1822d1550a8bc5e0d363026daa
John Okall <Jhnprimee@gmail.com>
2025-06-15 17:28:14 +0300

    feat: configure image domains and remote patterns in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      'stc.utdstc.com'
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'aiazrrsyklzrgjsp.public.blob.vercel-storage.com',
+        port: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
